bbb3d4a2cedf924a903714b1e1b80ea14787512f
SignatureDom <71554049+SignatureDom@users.noreply.github.com>
2023-03-30 09:24:11 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "GO") {
+        kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1, kitronik_motor_driver.MotorDirection.Forward, 50)
+        kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Reverse, 50)
+    } else if (receivedString == "BACK") {
+
+        kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1, kitronik_motor_driver.MotorDirection.Reverse, 50)
+        kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Forward, 50)
+    
+    } else {
+        kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor1)
+        kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor2)
+    }
+})
+// Start music
+music.playTone(988, music.beat(BeatFraction.Sixteenth))
+// Radio setGroup
+radio.setGroup(198)
